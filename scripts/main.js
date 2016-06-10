317fe04e277b4cde3071cc9133f2826c167d5649
Claudine <claudine.bananal@gmail.com>
2016-06-10 00:17:14 -0700

    incorporate fixed header/navigation on top of the page

--- scripts/main.js
+++ scripts/main.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
 	    var $target = $(target);
 
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
+	        'scrollTop': $target.offset().top -76
+	    }, 1000, 'swing', function () {
 	        window.location.hash = target;
 	    });
 	});
